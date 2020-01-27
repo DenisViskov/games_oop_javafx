76518ef44d572696306efd9e23dc21f745f6654b
DenisViskov <denisviskov16@gmail.com>
2020-01-27 17:01:18 +0300

    update

--- tictactoe/src/main/java/job4j/tictactoe/Logic3T.java
+++ tictactoe/src/main/java/job4j/tictactoe/Logic3T.java
@@ -1,43 +1,58 @@
 package job4j.tictactoe;
 
-import java.util.function.Predicate;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
 
 public class Logic3T {
+    /**
+     * Map of table
+     */
     private final Figure3T[][] table;
 
     public Logic3T(Figure3T[][] table) {
         this.table = table;
     }
 
-    public boolean fillBy(Predicate<Figure3T> predicate, int startX, int startY, int deltaX, int deltaY) {
-        boolean result = true;
-        for (int index = 0; index != this.table.length; index++) {
-            Figure3T cell = this.table[startX][startY];
-            startX += deltaX;
-            startY += deltaY;
-            if (!predicate.test(cell)) {
-                result = false;
-                break;
-            }
-        }
-        return result;
-    }
-
+    /**
+     * Method realizes serching of winner X
+     *
+     * @return - true or false
+     */
     public boolean isWinnerX() {
-        return this.fillBy(Figure3T::hasMarkX, 0, 0, 1, 0) ||
-                this.fillBy(Figure3T::hasMarkX, 0, 0, 0, 1) ||
-                this.fillBy(Figure3T::hasMarkX, 0,0, 1, 1) ||
-                this.fillBy(Figure3T::hasMarkX, this.table.length - 1 , 0, -1, 1);
+        return hasCombination('x');
     }
 
+    /**
+     * Method realizes serching of winner O
+     *
+     * @return - true or false
+     */
     public boolean isWinnerO() {
-        return this.fillBy(Figure3T::hasMarkO, 0, 0, 1, 0) ||
-                this.fillBy(Figure3T::hasMarkO, 0, 0, 0, 1) ||
-                this.fillBy(Figure3T::hasMarkO, 0,0, 1, 1) ||
-                this.fillBy(Figure3T::hasMarkO, this.table.length - 1, 0, -1, 1);
+        return false;
     }
 
+    /**
+     * Method realizes serching of gaps in table
+     *
+     * @return
+     */
     public boolean hasGap() {
+        return Arrays.stream(this.table)
+                .flatMap(e -> Stream.of(e))
+                .anyMatch(e -> !e.hasMarkO() && !e.hasMarkX());
+    }
+
+    /**
+     * Method has realizes searching of winner 4 combinations of turns
+     *
+     * @return - true or false
+     */
+    public boolean hasCombination(char figure) {
+        List<Boolean> conditions = List.of(Arrays.stream(this.table)
+                .anyMatch(e -> Stream.of(e).allMatch(i -> figure == 'x' ? i.hasMarkX() : i.hasMarkO())));
+
         return true;
     }
 }

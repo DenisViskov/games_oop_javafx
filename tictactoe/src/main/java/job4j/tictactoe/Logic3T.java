fcdc1d0a7aad0e76dedb5576d37bacc5ebe128e1
DenisViskov <DenisViskov16@gmail.com>
2020-01-29 10:15:13 +0300

    update

--- tictactoe/src/main/java/job4j/tictactoe/Logic3T.java
+++ tictactoe/src/main/java/job4j/tictactoe/Logic3T.java
@@ -4,6 +4,7 @@ package job4j.tictactoe;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Predicate;
 import java.util.stream.Stream;
 
 public class Logic3T {
@@ -22,7 +23,10 @@ public class Logic3T {
      * @return - true or false
      */
     public boolean isWinnerX() {
-        return hasCombination('x');
+        return this.fillBy(Figure3T::hasMarkX, 0, 0, 1, 0) ||
+                this.fillBy(Figure3T::hasMarkX, 0, 0, 0, 1) ||
+                this.fillBy(Figure3T::hasMarkX, 0, 0, 1, 1) ||
+                this.fillBy(Figure3T::hasMarkX, this.table.length - 1, 0, -1, 1);
     }
 
     /**
@@ -31,7 +35,10 @@ public class Logic3T {
      * @return - true or false
      */
     public boolean isWinnerO() {
-        return hasCombination('o');
+        return this.fillBy(Figure3T::hasMarkO, 0, 0, 1, 0) ||
+                this.fillBy(Figure3T::hasMarkO, 0, 0, 0, 1) ||
+                this.fillBy(Figure3T::hasMarkO, 0, 0, 1, 1) ||
+                this.fillBy(Figure3T::hasMarkO, this.table.length - 1, 0, -1, 1);
     }
 
     /**
@@ -46,47 +53,25 @@ public class Logic3T {
     }
 
     /**
-     * Method has realizes searching of winner 4 combinations of turns
+     * Method has realize serching of sequence
      *
+     * @param predicate - performs check of mark
+     * @param startX    - start position
+     * @param startY    - end position
+     * @param deltaX    - move
+     * @param deltaY    - move
      * @return - true or false
      */
-    public boolean hasCombination(char figure) {
-        List<Boolean> conditions = List.of(Arrays.stream(this.table)
-                        .anyMatch(e -> Stream.of(e)
-                                .allMatch(i -> figure == 'x' ? i.hasMarkX() : i.hasMarkO())),
-                Arrays.stream(tableChanger("vertical"))
-                        .anyMatch(e -> Stream.of(e)
-                                .allMatch(i -> figure == 'x' ? i.hasMarkX() : i.hasMarkO())),
-                Arrays.stream(tableChanger("diagonal"))
-                        .anyMatch(e -> Stream.of(e)
-                                .allMatch(i -> figure == 'x' ? i.hasMarkX() : i.hasMarkO())));
-        return conditions.contains(true) ? true : false;
-    }
-
-    /**
-     * Method is an addon for hasCombination method
-     * He realizes operations of changes table on "vertical" or "diagonal"
-     *
-     * @param var - "vertical" or "diagonal"
-     * @return - specific array of parametres
-     */
-    private Figure3T[][] tableChanger(String var) {
-        Figure3T[][] result = new Figure3T[this.table.length][this.table.length];
-        if (var.equals("vertical")) {
-            for (int i = 0; i < this.table.length; i++) {
-                for (int j = 0; j < this.table.length; j++) {
-                    result[i][j] = this.table[j][i];
-                }
+    public boolean fillBy(Predicate<Figure3T> predicate, int startX, int startY, int deltaX, int deltaY) {
+        boolean result = true;
+        for (int index = 0; index != this.table.length; index++) {
+            Figure3T cell = this.table[startX][startY];
+            startX += deltaX;
+            startY += deltaY;
+            if (!predicate.test(cell)) {
+                result = false;
+                break;
             }
-        } else if (var.equals("diagonal")) {
-            return new Figure3T[][]{{this.table[0][0],
-                    this.table[1][1],
-                    this.table[2][2]},
-                    {this.table[2][0],
-                            this.table[1][1],
-                            this.table[0][2]}};
-        } else {
-            throw new IllegalArgumentException("Uncorrect parametres");
         }
         return result;
     }

183e1c3825a2946bf842a617111fa18ac9311234
DenisViskov <denisviskov16@gmail.com>
2020-01-31 11:20:34 +0300

    Крестики-нолики на JavaFX[#198170]

--- tictactoe/src/main/java/job4j/tictactoe/Logic3T.java
+++ tictactoe/src/main/java/job4j/tictactoe/Logic3T.java
@@ -23,14 +23,7 @@ public class Logic3T {
      * @return - true or false
      */
     public boolean isWinnerX() {
-        return this.fillBy(Figure3T::hasMarkX, 0, 0, 1, 0) ||
-                this.fillBy(Figure3T::hasMarkX, 0, 0, 0, 1) ||
-                this.fillBy(Figure3T::hasMarkX, 0, 0, 1, 1) ||
-                this.fillBy(Figure3T::hasMarkX, 0, 1, 1, 0) ||
-                this.fillBy(Figure3T::hasMarkX, 1, 0, 0, 1) ||
-                this.fillBy(Figure3T::hasMarkX, this.table.length - 1, 0, 0, 1) ||
-                this.fillBy(Figure3T::hasMarkX, 0, this.table.length - 1, 1, 0) ||
-                this.fillBy(Figure3T::hasMarkX, this.table.length - 1, 0, -1, 1);
+        return this.isWinner(Figure3T::hasMarkX);
     }
 
     /**
@@ -39,14 +32,7 @@ public class Logic3T {
      * @return - true or false
      */
     public boolean isWinnerO() {
-        return this.fillBy(Figure3T::hasMarkO, 0, 0, 1, 0) ||
-                this.fillBy(Figure3T::hasMarkO, 0, 0, 0, 1) ||
-                this.fillBy(Figure3T::hasMarkO, 0, 0, 1, 1) ||
-                this.fillBy(Figure3T::hasMarkO, 0, 1, 1, 0) ||
-                this.fillBy(Figure3T::hasMarkO, 1, 0, 0, 1) ||
-                this.fillBy(Figure3T::hasMarkO, this.table.length - 1, 0, 0, 1) ||
-                this.fillBy(Figure3T::hasMarkO, 0, this.table.length - 1, 1, 0) ||
-                this.fillBy(Figure3T::hasMarkO, this.table.length - 1, 0, -1, 1);
+        return this.isWinner(Figure3T::hasMarkO);
     }
 
     /**
@@ -82,5 +68,22 @@ public class Logic3T {
             }
         }
         return result;
+    }
+
+    /**
+     * Method realizes serching of winner
+     *
+     * @param predicate - what are we looking for ('x' or 'o')
+     * @return - true or false
+     */
+    private boolean isWinner(Predicate<Figure3T> predicate) {
+        return this.fillBy(predicate, 0, 0, 1, 0) ||
+                this.fillBy(predicate, 0, 0, 0, 1) ||
+                this.fillBy(predicate, 0, 0, 1, 1) ||
+                this.fillBy(predicate, 0, 1, 1, 0) ||
+                this.fillBy(predicate, 1, 0, 0, 1) ||
+                this.fillBy(predicate, this.table.length - 1, 0, 0, 1) ||
+                this.fillBy(predicate, 0, this.table.length - 1, 1, 0) ||
+                this.fillBy(predicate, this.table.length - 1, 0, -1, 1);
     }
 }
